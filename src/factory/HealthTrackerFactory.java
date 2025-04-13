8738a4dd15abf4e1aaace1cd94d5282aa44b3ad4
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2025-04-13 12:15:09 +0600

    added exercise option

--- src/factory/HealthTrackerFactory.java
+++ src/factory/HealthTrackerFactory.java
@@ -7,10 +7,11 @@ public class HealthTrackerFactory {
 
     public static HealthTracker getTracker(String type) {
         return switch (type.toLowerCase()) {
-            case "medical" -> new MedicalRecordTracker();
             case "diet" -> new DietTracker();
             case "sleep" -> new SleepTracker();
+            case "exercise" -> new ExerciseTracker();
 
+            case "medical" -> new MedicalRecordTracker();
             default -> throw new IllegalArgumentException("Unknown tracker type: " + type);
         };
     }
