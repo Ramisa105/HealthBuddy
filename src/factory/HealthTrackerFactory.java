c28e57cc9b1844d4914e17cd908021cbb20cc66d
Ramisa Anan Rahman <143859203+Ramisa105@users.noreply.github.com>
2025-04-13 11:26:33 +0600

    Updated the getTracker method to support diet

--- src/factory/HealthTrackerFactory.java
+++ src/factory/HealthTrackerFactory.java
@@ -1,13 +1,15 @@
 package factory;
 
 import interfaces.HealthTracker;
-import trackers.MedicalRecordTracker;
+import trackers.*;
 
 public class HealthTrackerFactory {
 
     public static HealthTracker getTracker(String type) {
         return switch (type.toLowerCase()) {
             case "medical" -> new MedicalRecordTracker();
+            case "diet" -> new DietTracker();
+
             default -> throw new IllegalArgumentException("Unknown tracker type: " + type);
         };
     }
